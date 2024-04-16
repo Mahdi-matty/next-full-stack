3864cc4fe7a39d48d80c1266a59ee0adde79bc3a
mahdi <mmiq69@gmail.com>
2024-04-16 16:11:51 -0400

    'toggle

--- components/nav.jsx
+++ components/nav.jsx
@@ -2,8 +2,18 @@
 
 import Link from 'next/link'
 import {signIn, signOut, useSession, getProviders} from 'next-auth/react'
+import { useEffect, useState } from 'react';
 
 export default function Nav(){
+    const [providers, setProviders] = useState(null)
+    const [toggleDropDown, setToggleDropDown] = useState(false)
+    useEffect(()=>{
+        const setProviders = async()=>{
+            const response= await getProviders();
+            setProviders(response)
+        }
+        setProviders()
+    }, [])
     const isLoggedIn = true;
     return (
         <nav className='flex-between w-full mb-16 pt-3'>
@@ -12,7 +22,30 @@ export default function Nav(){
                 {isLoggedIn ? (<div>
                     <Link href='/createpost'>create new post</Link>
                     <button onClick={signOut} className='outline_btn'>signOut</button>
-                </div>): (<div></div>)}
+                </div>): (<>
+                {providers && Object.values(providers).map((provider)=>(
+                    <button
+                    key={provider.name}
+                    onClick={()=>singIn(provider.id)}
+                    className='black_btn'>
+
+                    </button>
+                ))}
+                </>)}
+            </div>
+            <div className='sm:hidden flex relative'>
+                {isLoggedIn ? (<div>
+                    <h2>loged in</h2>
+                </div>): (<>
+                {providers && Object.values(providers).map((provider)=>(
+                    <button
+                    key={provider.name}
+                    onClick={()=>setToggleDropDown((prev)=>!prev)}
+                    className='black_btn'>
+
+                    </button>
+                ))}
+                </>)}
             </div>
         </nav>
 
